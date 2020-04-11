ea86bdc1a3c6f2a737ef1a41e6a15ac0b41f5501
Puyan Wei <puyanwei@gmail.com>
2020-04-11 11:35:21 +0100

    Example test showing bug on input type email

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("works", () => {
+    cy.visit("https://getbootstrap.com/docs/4.0/components/forms/");
+    cy.get("#exampleInputEmail1").type("one two three");
+  });
+});
